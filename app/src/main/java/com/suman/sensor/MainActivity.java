ab8a6e2e9012a83116ae78c97eb6364df3a9b1e1
SsumanShahi <shaggysy201118@gmail.com>
2020-01-17 07:40:48 +0545

    list of sensor shown

--- app/src/main/java/com/suman/sensor/MainActivity.java
+++ app/src/main/java/com/suman/sensor/MainActivity.java
@@ -2,13 +2,35 @@ package com.suman.sensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private TextView tvsen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvsen = findViewById(R.id.tvsen);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> sensorList =sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for(int i=0; i < sensorList.size(); i++)
+        {
+            String sensors ="";
+            sensors += sensorList.get(i).getName() + "\n";
+            tvsen.append(sensors);
+        }
+
     }
 }
